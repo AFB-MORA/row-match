01c796f08130e580bb32816bb2ef4230caa53428
Ahmet Furkan Bıyık <a.f.biyik@gmail.com>
2023-07-11 23:38:16 +0300

    implement prefab stage watcher

---
+++ Assets/Scripts/Com/AFBiyik/MatchRow/Global/Editor/UIPrefabStageWatcher.cs
@@ -0,0 +1,31 @@
+#if UNITY_EDITOR
+using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+/// <summary>
+/// Watches for state changes of prefab stage.
+/// Adds parent to ui prefabs.
+/// </summary>
+[InitializeOnLoad]
+static class UIPrefabStageWatcher
+{
+    static UIPrefabStageWatcher()
+    {
+        PrefabStage.prefabStageOpened += OnStageOpened;
+    }
+
+    private static void OnStageOpened(PrefabStage stage)
+    {
+        var prefab = stage.prefabContentsRoot;
+
+        if (prefab.GetComponent<RectTransform>() != null)
+        {
+            var rootObjects = stage.scene.GetRootGameObjects();
+            var parent = stage.scene.GetRootGameObjects().FirstOrDefault(go => go.name == "UIParent (Environment)");
+            prefab.transform.SetParent(parent.transform);
+        }
+    }
+}
+#endif
