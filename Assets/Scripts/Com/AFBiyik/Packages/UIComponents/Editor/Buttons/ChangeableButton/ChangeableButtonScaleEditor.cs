14bbac95bfb57de44a0844f8c5019aac22e428d1
Ahmet Furkan Bıyık <a.f.biyik@gmail.com>
2023-07-16 00:57:18 +0300

    update level button

---
+++ Assets/Scripts/Com/AFBiyik/Packages/UIComponents/Editor/Buttons/ChangeableButton/ChangeableButtonScaleEditor.cs
@@ -0,0 +1,10 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Com.AFBiyik.UIComponents
+{
+    [CustomPropertyDrawer(typeof(ChangeableButtonScale))]
+    public class ChangeableButtonScaleEditor : ChangeableButtonBaseEditor
+    {
+    }
+}
---
+++ Assets/Scripts/Com/AFBiyik/Packages/UIComponents/View/Buttons/ChangeableButton/ChangeableButtonScale.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+namespace Com.AFBiyik.UIComponents
+{
+    /// <summary>
+    /// Changes scale depends on <see cref="SelectionState"/>
+    /// </summary>
+    [Serializable]
+    public class ChangeableButtonScale : ChangeableButtonBase
+    {
+        // Public Fields
+        public Vector3 normal = new Vector3(1, 1, 1);
+        public Vector3 highlighted = new Vector3(1, 1, 1);
+        public Vector3 pressed = new Vector3(1, 1, 1);
+        public Vector3 selected = new Vector3(1, 1, 1);
+        public Vector3 disabled = new Vector3(1, 1, 1);
+
+        /// <inheritdoc/>
+        public override void SetSate(SelectionState state)
+        {
+            if (state == SelectionState.Normal)
+            {
+                selectable.transform.localScale = normal;
+            }
+            else if (state == SelectionState.Highlighted)
+            {
+                selectable.transform.localScale = highlighted;
+            }
+            else if (state == SelectionState.Pressed)
+            {
+                selectable.transform.localScale = pressed;
+            }
+            else if (state == SelectionState.Selected)
+            {
+                selectable.transform.localScale = selected;
+            }
+            else if (state == SelectionState.Disabled)
+            {
+                selectable.transform.localScale = disabled;
+            }
+        }
+    }
+}
