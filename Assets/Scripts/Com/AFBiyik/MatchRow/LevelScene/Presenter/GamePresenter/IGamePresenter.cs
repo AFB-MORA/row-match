0fabe16633d4729585956e15d2ca3c8b3a4c3e73
Ahmet Furkan Bıyık <a.f.biyik@gmail.com>
2023-07-17 16:39:44 +0300

    renaming

---
+++ Assets/Scripts/Com/AFBiyik/MatchRow/LevelScene/Presenter/GamePresenter/IGamePresenter.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using Com.AFBiyik.MatchRow.LevelScene.Enumeration;
+using UniRx;
+
+namespace Com.AFBiyik.MatchRow.LevelScene.Presenter
+{
+    public interface IGamePresenter
+    {
+        /// <summary>
+        /// Game Score
+        /// </summary>
+        IReadOnlyReactiveProperty<int> Score { get; }
+
+        /// <summary>
+        /// Game Score
+        /// </summary>
+        IReadOnlyReactiveProperty<int> HighScore { get; }
+
+        /// <summary>
+        /// Move count
+        /// </summary>
+        IReadOnlyReactiveProperty<int> MoveCount { get; }
+
+        /// <summary>
+        /// Game over event
+        /// </summary>
+        IObservable<object> OnGameOver { get; }
+
+        /// <summary>
+        /// True if high score changed; otherwise false
+        /// </summary>
+        bool IsHighScore { get; }
+
+        /// <summary>
+        /// Animating Views
+        /// </summary>
+        HashSet<Guid> AnimatingViews { get; set; }
+
+        /// <summary>
+        /// Updates score
+        /// </summary>
+        /// <param name="itemType">Row item type</param>
+        /// <param name="columns">Number of columns</param>
+        void UpdateScore(ItemType itemType, int columns);
+
+        /// <summary>
+        /// Decreases move count.
+        /// </summary>
+        /// <returns>True if has move; otherwise false.</returns>
+        bool MakeMove();
+
+        /// <summary>
+        /// Sets game over
+        /// </summary>
+        void GameOver();
+    }
+}
---
+++ Assets/Scripts/Com/AFBiyik/MatchRow/LevelScene/View/UI/HighScoreView.cs
@@ -0,0 +1,85 @@
+using System;
+using Com.AFBiyik.MatchRow.LevelScene.VFX;
+using Cysharp.Threading.Tasks;
+using UniRx;
+using UnityEngine;
+
+namespace Com.AFBiyik.MatchRow.LevelScene.View
+{
+    /// <summary>
+    /// Displays score
+    /// </summary>
+    public class HighScoreView : BaseValueView
+    {
+        // Private Fields
+        [SerializeField]
+        private GameObject highScoreEffect;
+        [SerializeField]
+        private float lifeTime;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            highScoreEffect.SetActive(false);
+        }
+
+        protected override void SubscribeValue()
+        {
+            // Subscrive score value
+            gamePresenter.HighScore
+                .TakeUntilDestroy(gameObject)
+                .Subscribe(OnValueChange);
+        }
+
+        protected override async UniTask OnValueChangeAsync(int newValue)
+        {
+            // Add to animating views
+            Guid animation = Guid.NewGuid();
+            gamePresenter.AnimatingViews.Add(animation);
+
+            bool initial = isInitial;
+
+            if (!initial)
+            {
+                // Wait move animation
+                await UniTask.Delay((int)(CompletedEffect.SCORE_MOVE_TWEEN_TIME * 1000));
+            }
+
+            // Change value
+            UniTask baseTask = base.OnValueChangeAsync(newValue);
+
+            // If not initial
+            if (!initial)
+            {
+                await UniTask.WhenAll(ShowEffect(),
+                    BaseTask(baseTask, animation)
+                );
+            }
+            else
+            {
+                await BaseTask(baseTask, animation);
+            }
+        }
+
+        private async UniTask BaseTask(UniTask baseTask, Guid animation)
+        {
+            await baseTask;
+
+            await UniTask.Delay((int)(lifeTime * 1000 * 0.3f));
+
+            // Remove from animating views
+            gamePresenter.AnimatingViews.Remove(animation);
+        }
+
+        private async UniTask ShowEffect()
+        {
+            // Show effect
+            highScoreEffect.SetActive(true);
+
+            await UniTask.Delay((int)(lifeTime * 1000));
+
+            // Hide effect
+            highScoreEffect.SetActive(false);
+        }
+    }
+}
